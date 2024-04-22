b4a00276750c37f06fe44c6ff6845be2d9f01150
Marcus Vukojevic <marcus.vukojevic@studenti.unitn.it>
2024-04-22 17:27:56 +0200

    kn

--- da_consegnare/main.py
+++ da_consegnare/main.py
@@ -19,6 +19,14 @@ from functions import init_weights, train_loop, eval_loop
 DEVICE = 'cuda:0'
 
 
+"""
+
+In generare, la pipeline usata per lo sviluppo è uguale a quella del notebook originale, l'unica cosa che è stata cambiata sono
+i hyperparameters, quale modello usare o quale ottimizzatore. Tutto è stato riscritto in modo molto semplice e si può verificare 
+tutti i parametri usati. Gli hyperparams usati per il miglior esperimento sono gli stessi usati in questo file
+
+"""
+
 cartella_risultati = "risultati"
 
 os.makedirs(f'{cartella_risultati}', exist_ok=True)
@@ -62,11 +70,10 @@ test_loader = DataLoader(test_dataset, batch_size=128, collate_fn=partial(collat
 # secondo era: variational dropout --> LM_LSTM_2_COMPLETA
 # terzo era: nt-avsgd
 
-architettura = ["lstm_no_drop", "lstm", 'lstm-wt', 'lstm-completa']
-loss_model = ["sgd" , "adam", "nt-avsgd"]
 
-parametri = [300, 300]
-learning_rate = 1
+
+parametri = [600, 600] # hidden e embedding size
+learning_rate = 10
 
 clip = 5 # Clip the gradient
 vocab_len = len(lang.word2id)
@@ -74,6 +81,9 @@ vocab_len = len(lang.word2id)
 n_epochs = 100
 patience = 6
 
+# Dizionario che contiene tutti gli esperimenti che verrano eseguiti in serie
+# per ogni oesperimento, dichiaro quale modello uso, quale otimizzatore e il titolo dell'esperimento
+# per far in modo di passare gli stessi parametri del modello, nel blocco successivo venogno dichiarate le funzioni di ottimizzazione
 assignments = {
     "1" : [LM_LSTM_NO_DROP(parametri[1], parametri[0], vocab_len, pad_index=lang.word2id["<pad>"]), None , "RNN->LSTM"],
     "2": [LM_LSTM(parametri[1], parametri[0], vocab_len, pad_index=lang.word2id["<pad>"]), None , "LSTM+DROP"],
@@ -85,7 +95,7 @@ assignments = {
 
 assignments["1"][1] = optim.SGD(assignments["1"][0].parameters(), lr=learning_rate)
 assignments["2"][1] = optim.SGD(assignments["2"][0].parameters(), lr=learning_rate)
-assignments["3"][1] = optim.AdamW(assignments["3"][0].parameters(), lr=0.001) # dopo esperimento 1: abbasso learning rate per Adam
+assignments["3"][1] = optim.AdamW(assignments["3"][0].parameters(), lr=0.001) # dopo esperimento 1: abbasso learning rate per Adam perché performance non buona
 assignments["4"][1] = optim.SGD(assignments["4"][0].parameters(), lr=learning_rate)
 assignments["5"][1] = optim.SGD(assignments["5"][0].parameters(), lr=learning_rate)
 assignments["6"][1] = Nt_AvSGD(assignments["6"][0].parameters(), lr=learning_rate, n=5)
@@ -93,14 +103,20 @@ assignments["6"][1] = Nt_AvSGD(assignments["6"][0].parameters(), lr=learning_rat
 
 for i in range(1,7):
 
+    # lista dei learning rates usati durante l'allenamento
     learning_rates = []
+
     print("Esperimento: ", assignments[f"{i}"][2])
+
+    # chiamo il mio modello e ottimizzatore
     model = assignments[f"{i}"][0].to(DEVICE)
     model.apply(init_weights)
 
     optimizer = assignments[f"{i}"][1]
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=3, factor=0.1) # così mi cambia la lr in modo automatico
-    #scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.95)
+
+    # Dichiaro lo scheduler che mi andrà ad adattare la learning rate durante il traning, alcuni sono stati provati
+    # scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=3, factor=0.1) # così mi cambia la lr in modo automatico
+    scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.95)
 
 
     criterion_train = nn.CrossEntropyLoss(ignore_index=lang.word2id["<pad>"])
@@ -111,6 +127,7 @@ for i in range(1,7):
     best_ppl = math.inf
     best_model = None
     pbar = tqdm(range(1,n_epochs))
+
     #If the PPL is too high try to change the learning rate
     for epoch in pbar:
         loss = train_loop(train_loader, optimizer, criterion_train, model, clip)
@@ -121,11 +138,12 @@ for i in range(1,7):
 
             # tengo traccia delle learning rates
             for param_group in optimizer.param_groups:
-                #print(f"Epoch {epoch+1}, Current LR: {param_group['lr']}")
                 learning_rates.append(f"Epoch {epoch+1}, Current LR: {param_group['lr']}")
     
             if i != 3:
-                scheduler.step(loss_dev) #chiamo lo step dello scheduler
+                scheduler.step(loss_dev) #chiamo lo step dello scheduler per tutti tranne che per AdamW
+            
+            # solo per l'ultimo esperimento ho bisogno di chiamare la funzione check, perché sto usando il mio ottimizzatore
             if i == 6:
                 optimizer.check(ppl_dev)
             
@@ -144,7 +162,7 @@ for i in range(1,7):
     best_model.to(DEVICE)
     final_ppl,  _ = eval_loop(test_loader, criterion_eval, best_model)
 
-    
+    # Salvo in un file i miei risultati e il modello risultante
     print(f'Test PPL for {assignments[f"{i}"][2]} --> ', final_ppl)
     # Save the model
     model_path = f'modelli/esperimento_{multipli_di_6}_{assignments[f"{i}"][2]}.pt'
