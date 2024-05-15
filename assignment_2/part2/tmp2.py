95b43f032925428f124965d9245811b6c741187a
Marcus Vukojevic <marcus.vukojevic@studenti.unitn.it>
2024-05-15 16:20:26 +0200

    fpkn

--- assignment_2/part2/tmp2.py
+++ assignment_2/part2/tmp2.py
@@ -99,60 +99,61 @@ runs = 5
 slot_f1s, intent_acc = [], []
 
 
+for run in range(5):
+    modello = BertFineTune(model, intent_len, slot_len, device=device).to(device)
+    optimizer = optim.Adam(modello.parameters(), lr=lr)
 
-modello = BertFineTune(model, intent_len, slot_len, device=device).to(device)
-optimizer = optim.Adam(modello.parameters(), lr=lr)
+    criterion_slots = nn.CrossEntropyLoss(ignore_index=PAD_TOKEN)
+    criterion_intents = nn.CrossEntropyLoss(ignore_index=PAD_TOKEN)
 
-criterion_slots = nn.CrossEntropyLoss(ignore_index=PAD_TOKEN)
-criterion_intents = nn.CrossEntropyLoss(ignore_index=PAD_TOKEN)
+    patience = 3
+    losses_train = []
+    losses_dev = []
+    sampled_epochs = []
+    best_f1 = 0
+    for x in range(1,n_epochs):
+        print("N_epoch:", x)
+        loss = train_loop(train_loader, optimizer, criterion_slots, 
+                            criterion_intents, modello)
+        if x % 2 == 0:
+            sampled_epochs.append(x)
+            losses_train.append(np.asarray(loss).mean())
+            results_dev, intent_res, loss_dev = eval_loop(dev_loader, criterion_slots, 
+                                                            criterion_intents, modello, lang, tokenizer)
+            losses_dev.append(np.asarray(loss_dev).mean())
+            f1 = results_dev['total']['f']
 
-patience = 3
-losses_train = []
-losses_dev = []
-sampled_epochs = []
-best_f1 = 0
-for x in range(1,n_epochs):
-    print("N_epoch:", x)
-    loss = train_loop(train_loader, optimizer, criterion_slots, 
-                        criterion_intents, modello)
-    if x % 2 == 0:
-        sampled_epochs.append(x)
-        losses_train.append(np.asarray(loss).mean())
-        results_dev, intent_res, loss_dev = eval_loop(dev_loader, criterion_slots, 
-                                                        criterion_intents, modello, lang, tokenizer)
-        losses_dev.append(np.asarray(loss_dev).mean())
-        f1 = results_dev['total']['f']
+            if f1 > best_f1:
+                best_f1 = f1
+            else:
+                patience -= 1
+            if patience <= 0: # Early stopping with patient
+                break # Not nice but it keeps the code clean
 
-        if f1 > best_f1:
-            best_f1 = f1
-        else:
-            patience -= 1
-        if patience <= 0: # Early stopping with patient
-            break # Not nice but it keeps the code clean
+    results_test, intent_test, _ = eval_loop(test_loader, criterion_slots, 
+                                                criterion_intents, modello, lang, tokenizer)
+    intent_acc.append(intent_test['accuracy'])
+    slot_f1s.append(results_test['total']['f'])
+    print("gugu")
+    print(results_test['total']['f'])
+    PATH = os.path.join("bin", "model_1")
+    if not os.path.exists(os.path.dirname(PATH)):
+        os.makedirs(os.path.dirname(PATH))
+    saving_object = {"epoch": x, 
+                    "model": model.state_dict(), 
+                    "optimizer": optimizer.state_dict(), 
+                    "slot2id": lang.slot2id, 
+                    "intent2id": lang.intent2id}
 
-results_test, intent_test, _ = eval_loop(test_loader, criterion_slots, 
-                                            criterion_intents, modello, lang, tokenizer)
-intent_acc.append(intent_test['accuracy'])
-slot_f1s.append(results_test['total']['f'])
-print("gugu")
-print(results_test['total']['f'])
-#PATH = os.path.join("bin", "model_1")
-#saving_object = {"epoch": x, 
-#                "model": model.state_dict(), 
-#                "optimizer": optimizer.state_dict(), 
-#                "w2id": w2id, 
-#                "slot2id": slot2id, 
-#                "intent2id": intent2id}
-#torch.save(saving_object, PATH)
-
-#plt.figure(num = 3, figsize=(8, 5)).patch.set_facecolor('white')
-#plt.title('Train and Dev Losses')
-#plt.ylabel('Loss')
-#plt.xlabel('Epochs')
-#plt.plot(sampled_epochs, losses_train, label='Train loss')
-#plt.plot(sampled_epochs, losses_dev, label='Dev loss')
-#plt.legend()
-#plt.show()
+    torch.save(saving_object, PATH)
+    plt.figure(num = 3, figsize=(8, 5)).patch.set_facecolor('white')
+    plt.title('Train and Dev Losses')
+    plt.ylabel('Loss')
+    plt.xlabel('Epochs')
+    plt.plot(sampled_epochs, losses_train, label='Train loss')
+    plt.plot(sampled_epochs, losses_dev, label='Dev loss')
+    plt.legend()
+    plt.show()
 
 
 # printa il calcolo finale
