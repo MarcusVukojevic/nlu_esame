879fd75cc64c814cc2d8d6572fff65b900972304
Marcus Vukojevic <marcus.vukojevic@studenti.unitn.it>
2024-05-18 17:22:56 +0200

    ln

--- assignment_3/main.py
+++ assignment_3/main.py
@@ -18,7 +18,7 @@ from models import Lang, TokensAndLabels, BertFineTune
 from functions import train_loop, eval_loop
 from transformers import BertTokenizer, BertModel
 
-device = 'mps:0' # cuda:0 means we are using the GPU with id 0, if you have multiple GPU
+device = 'cuda:0' # cuda:0 means we are using the GPU with id 0, if you have multiple GPU
 
 os.environ['CUDA_LAUNCH_BLOCKING'] = "1" # Used to report errors on CUDA side
 PAD_TOKEN = 0
